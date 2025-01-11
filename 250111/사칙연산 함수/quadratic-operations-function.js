f68c401f9f3dbdae35077dbdf0f12af8e6db8318
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-01-11 11:21:57 +0900

    Title: 사칙연산 함수, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250111/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+const [a,o,c] = fs.readFileSync(0).toString().trim().split(" ")
+const numA = Number(a)
+const numC = Number(c)
+let answer = `${numA} ${o} ${numC} = `
+
+if(o === "+"){
+    answer += numA + numC
+}
+else if(o === "-"){
+    answer += numA - numC
+}
+else if(o === "/"){
+    answer += numA / numC
+}
+else if(o === "*"){
+    answer += numA * numC
+}
+else{
+    answer = "False"
+}
+console.log(answer)
