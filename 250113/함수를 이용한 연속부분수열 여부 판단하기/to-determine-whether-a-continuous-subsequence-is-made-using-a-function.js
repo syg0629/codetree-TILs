970e22f24a13942273f65a391406e24ec631f360
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-01-13 23:57:37 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 174ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250113/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
+++ 250113/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
@@ -1,9 +1,12 @@
 const fs = require("fs")
-const [_, a, b] = fs.readFileSync(0).toString().trim().split("\n").map((x)=> x.split(" "))
+const [n, a, b] = fs.readFileSync(0).toString().trim().split("\n").map((x)=> x.split(" "))
+const [numA, numB] = n
+
 const aStr = a.join(",")
 const bStr = b.join(",")
-if(aStr.includes(bStr)){
-    console.log("Yes")
-}else{
-    console.log("No")
-}a
+
+if (numA > numB) {
+    console.log(aStr.includes(bStr) ? "Yes" : "No");
+} else {
+    console.log(bStr.includes(aStr) ? "Yes" : "No");
+}
