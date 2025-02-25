28d24b0266bb1dc1c183e04810308ec6248f3463
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-02-25 16:06:51 +0900

    Title: 두 개의 동일한 수열, Time: 217ms, Memory: 15MB, Status: Passed - CodeTree

--- 250225/두 개의 동일한 수열/two-equal-series.js
+++ 250225/두 개의 동일한 수열/two-equal-series.js
@@ -1,25 +1,5 @@
 const fs = require("fs")
 const input = fs.readFileSync(0).toString().trim().split("\n")
-// 수열 A
 const A = input[1].trim().split(" ").map(Number).sort((a,b)=> a-b)
-// 수열 B
 const B = input[2].trim().split(" ").map(Number).sort((a,b)=> a-b)
-
-// if(Object.is(A,B)) {
-//     console.log("YES")
-// }
-
-// if(A.length !== B.length) {
-//     console.log("No")
-//     return 
-// }
-
-// for (let i = 0; i < A.length; i++) {
-//     if(!(B.some((b) => b == A[i])) ) {
-//         console.log("No")
-//         return 
-//     }
-// }
-
-// console.log("Yes")
 console.log(A.some((v, i) => v !== B[i]) ? "No" : "Yes")
