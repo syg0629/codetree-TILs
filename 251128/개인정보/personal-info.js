8ec193d5d966eb8d12e87f84de66d11fbafc0823
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-11-28 13:08:40 +0900

    Title: 개인정보, Time: 79ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 251128/개인정보/personal-info.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const students = [];
+for (let i = 0; i < 5; i++) {
+  const [name, heightStr, weightStr] = input[i].split(' ');
+  const height = Number(heightStr);
+  const weight = Number(weightStr);
+  students.push({ name, height, weight });
+}
+
+const nameSort = [...students].sort((a,b)=> {
+    return a.name.localeCompare(b.name)
+})
+
+const heightSort = [...students].sort((a,b)=> {
+  return b.height - a.height
+})
+
+console.log("name")
+for(const x of nameSort){
+  console.log(x.name, x.height, x.weight)
+}
+console.log("")
+console.log("height")
+for(const x of heightSort){
+  console.log(x.name, x.height, x.weight)
+}
