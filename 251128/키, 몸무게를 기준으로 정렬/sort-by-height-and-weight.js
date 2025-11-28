bcfe14d58a0f2982298f7d966e0d1b07f2892f79
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-11-28 23:50:18 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251128/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0]);
+const records = [];
+for (let i = 1; i <= n; i++) {
+  const [name, height, weight] = input[i].split(" ");
+  records.push([name, Number(height), Number(weight)]);
+}
+
+records.sort((a,b)=>{
+    if(a[1] !== b[1]){
+        return a[1] - b[1]
+    }
+    return b[2]-a[2]
+})
+
+for(const x of records){
+    console.log(x.join(" "))
+}
