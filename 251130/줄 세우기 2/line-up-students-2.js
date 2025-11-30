777a26aa06fc1de682d0e3286eb206a02d1f1165
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-11-30 23:55:24 +0900

    Title: 줄 세우기 2, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 251130/줄 세우기 2/line-up-students-2.js
+++ 251130/줄 세우기 2/line-up-students-2.js
@@ -5,19 +5,19 @@ const n = Number(input[0]);
 const studentInputs = [];
 for (let i = 1; i <= n; i++) {
     const [height, weight] = input[i].split(' ').map(Number);
-    studentInputs.push([height, weight]);
+    studentInputs.push([height, weight, i]);
 }
 
-studentInputs.sort((a,b)=> {
-    if(a[0]!==b[0]){
-        return a[0]-b[0]
+studentInputs.sort((a, b) => {
+    if (a[0] !== b[0]) {
+        return a[0] - b[0];
     }
-    if(a[1]!==b[1]){
-        return b[1]-a[1]
-    }    
+    if (a[1] !== b[1]) {
+        return b[1] - a[1];
+    }
 })
 
 for (let i = 0; i < studentInputs.length; i++) {
-    const [height, weight] = studentInputs[i];
-    console.log(`${height} ${weight} ${i + 1}`);
+    const [height, weight, originalOrder] = studentInputs[i];
+    console.log(`${height} ${weight} ${originalOrder}`);
 }
