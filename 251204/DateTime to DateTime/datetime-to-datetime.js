29ddf89a532dae35d95203536b358cd55c42bc98
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-12-04 18:47:53 +0900

    Title: DateTime to DateTime, Time: 74ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251204/DateTime to DateTime/datetime-to-datetime.js
+++ 251204/DateTime to DateTime/datetime-to-datetime.js
@@ -8,16 +8,20 @@ const hour = 11
 const min = 11
 let totalMin = 0
 
-if(day > A || day === A && hour < B || day === A && hour === B && min < C) console.log(-1)
-if(day < A){
-    const totalDay = A - day
-    totalMin += totalDay * 24 * 60
+if(day > A || day === A && hour < B || day === A && hour === B && min < C) {
+    console.log(-1)
 }
-if(hour < B){
-    const totalHour = B - hour
-    totalMin += totalHour * 60
+else{
+    if(day < A){
+        const totalDay = A - day
+        totalMin += totalDay * 24 * 60
+    }
+    if(hour < B){
+        const totalHour = B - hour
+        totalMin += totalHour * 60
+    }
+    if(min < C){
+        totalMin += C - min
+    }   
+    console.log(totalMin)
 }
-if(min < C){
-    totalMin += C - min
-}   
-console.log(totalMin)
