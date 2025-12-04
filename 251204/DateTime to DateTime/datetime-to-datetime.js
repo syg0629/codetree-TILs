755228b24426d2b62a3e361e9600ee3b2dfdbb5b
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-12-04 18:46:12 +0900

    Title: DateTime to DateTime, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251204/DateTime to DateTime/datetime-to-datetime.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [A, B, C] = input[0].split(' ').map(Number);
+
+const day = 11
+const hour = 11
+const min = 11
+let totalMin = 0
+
+if(day > A || day === A && hour < B || day === A && hour === B && min < C) console.log(-1)
+if(day < A){
+    const totalDay = A - day
+    totalMin += totalDay * 24 * 60
+}
+if(hour < B){
+    const totalHour = B - hour
+    totalMin += totalHour * 60
+}
+if(min < C){
+    totalMin += C - min
+}   
+console.log(totalMin)
