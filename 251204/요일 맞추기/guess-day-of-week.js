bf4be836f801e6956b374c6dea224f4a9133bf58
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-12-04 23:02:28 +0900

    Title: 요일 맞추기, Time: 86ms, Memory: 10MB, Status: Passed - Codetree

--- 251204/요일 맞추기/guess-day-of-week.js
+++ 251204/요일 맞추기/guess-day-of-week.js
@@ -5,10 +5,7 @@ const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
 const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 const date1 = new Date(2011, m1-1, d1)
 const date2 = new Date(2011, m2-1, d2)
-if(date1 < date2){
-    const dayIdx = date1.getDay()-date2.getDay()
-    console.log(day[1 + dayIdx])
-}else{
-    const dayIdx = date1.getDay()-date2.getDay()
-    console.log(day[1 - dayIdx])
-}
+
+const dayDiff = date2.getDay() - date1.getDay();
+const resultIdx = (1 + dayDiff + 7) % 7;
+console.log(day[resultIdx])
