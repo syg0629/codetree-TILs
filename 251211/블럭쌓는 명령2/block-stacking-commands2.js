b412fd9d5c16d0823f60739248cf09b5b439d021
Jiyu <73331052+syg0629@users.noreply.github.com>
2025-12-11 00:15:09 +0900

    Title: 블럭쌓는 명령2, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 251211/블럭쌓는 명령2/block-stacking-commands2.js
+++ 251211/블럭쌓는 명령2/block-stacking-commands2.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].split(' ').map(Number);
 const segments = input.slice(1, k + 1).map(line => line.split(' ').map(Number));
 
-const arr = new Array(n).fill(0);
+const arr = new Array(n+1).fill(0);
 
 for(const [from, to] of segments) {
     for(let i = from; i <= to; i++){
