1da7e7dcb8d1e95d9e16c9aae220a57afe6523d6
Jiyu <73331052+syg0629@users.noreply.github.com>
2026-01-16 22:25:33 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 194ms, Memory: 59MB, Status: Passed - Codetree

--- 260116/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.js
+++ 260116/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.js
@@ -5,11 +5,11 @@ const rectA = input[0].split(' ').map(Number);
 const rectB = input[1].split(' ').map(Number);
 const rectM = input[2].split(' ').map(Number);
 
-const arr = Array(1001).fill(0).map(() => Array(1001).fill(0))
+const arr = Array(2001).fill(0).map(() => Array(2001).fill(0))
 for (const [x1, y1, x2, y2] of [rectA, rectB]) {
     for (let x = x1; x < x2; x++) {
         for (let y = y1; y < y2; y++) {
-            arr[x][y] = 1
+            arr[x+1000][y+1000] = 1
         }
     }
 }
@@ -17,14 +17,14 @@ for (const [x1, y1, x2, y2] of [rectA, rectB]) {
 const [x1, y1, x2, y2] = rectM
 for (let x = x1; x < x2; x++) {
     for (let y = y1; y < y2; y++) {
-        arr[x][y] = 0
+        arr[x+1000][y+1000] = 0
     }
 }
 
 
 let answer = 0
-for (let i = 0; i < 1001; i++) {
-    for (let j = 0; j < 1001; j++) {
+for (let i = 0; i < 2001; i++) {
+    for (let j = 0; j < 2001; j++) {
         answer += arr[i][j]
     }
 }
